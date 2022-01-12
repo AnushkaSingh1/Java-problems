18a4164c516860b509e561fdfad6fad26ce0d849
Anushka Singh <anushkasingh117733@gmail.com>
2022-01-12 16:37:32 +0530

    Create duplicate values string.java
    Write a Java program to find the duplicate values of an array of string values.

---
+++ duplicate values string.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args) 
+    {
+        String[] my_array = {"bc", "ab", "ju", "bc", "oi", "gz", "oi"};
+ 
+        for (int i = 0; i < my_array.length-1; i++)
+        {
+            for (int j = i+1; j < my_array.length; j++)
+            {
+                if( (my_array[i].equals(my_array[j])) && (i != j) )
+                {
+                    System.out.println("Duplicate Element is : "+my_array[j]);
+                }
+            }
+        }
+    }    
+}
