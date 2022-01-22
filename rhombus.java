b8066f2bc2a05973e312d73f629baa859fcdf258
Anushka Singh <anushkasingh117733@gmail.com>
2022-01-22 17:59:57 +0530

    Create rhombus.java

---
+++ rhombus.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String args[]) 
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the number:  ");
+        int n = in.nextInt();
+        int count = 1;
+        int no_of_spaces = 1;
+        int start = 0;
+
+        for (int i = 1; i < (n * 2); i++) 
+        {
+
+            for (int spc = n - no_of_spaces; spc > 0; spc--) 
+            {
+                System.out.print(" ");
+            }
+            if (i < n) 
+            {
+                start = i;          
+                no_of_spaces++;    
+            } else 
+            {
+                start = n * 2 - i;   
+                no_of_spaces--;      
+            }
+            for (int j = 0; j < count; j++) 
+            {
+                System.out.print(start);
+                if (j < count / 2) 
+                {
+                    start--;
+                } else 
+                {
+                    start++;
+                }
+            }
+            if (i < n)
+            {
+                count = count + 2;
+            } else {
+                count = count - 2;
+            }
+
+            System.out.println();
+        }
+    }
+}
