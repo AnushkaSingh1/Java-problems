e3ea8983d5fd7e8ca01fc50fa7c475c62091d1c6
Anushka Singh <anushkasingh117733@gmail.com>
2022-02-03 19:23:52 +0530

    Update member.java

--- member.java
+++ member.java
@@ -36,3 +36,11 @@ class Member {
         this.department = department;
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Sam", 25, "555-555-55", "Home", 25631.5, "IT");
+        Manager manager = new Manager("Anny", 30, "555-617-55", "Earth", 69586.5, "IT");
+        employee.printSalary();
+        manager.printSalary();
+    }
+}
