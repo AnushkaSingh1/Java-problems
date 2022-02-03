d20d54c4ce6c4a39b88a0e8ffa873c79fc01e96d
Anushka Singh <anushkasingh117733@gmail.com>
2022-02-03 19:24:55 +0530

    Update member.java

--- member.java
+++ member.java
@@ -44,3 +44,17 @@ public class Main {
         manager.printSalary();
     }
 }
+
+
+
+
+
+//Create a class named 'Member' having the following members: 
+Data members 
+1 - Name 
+2 - Age 
+3 - Phone number 
+4 - Address 
+5 - Salary 
+It also has a method named 'printSalary' which prints the salary of the members. 
+Two classes 'Employee' and 'Manager' inherits the 'Member' class. The 'Employee' and 'Manager' classes have data members 'specialization' and 'department' respectively. Now, assign name, age, phone number, address and salary to an employee and a manager by making an object of both of these classes and print the same. 
