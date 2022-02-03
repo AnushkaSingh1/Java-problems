a0735e70a69aa65498f58032a888fd1fb3184467
Anushka Singh <anushkasingh117733@gmail.com>
2022-02-03 19:22:28 +0530

    Create member.java

---
+++ member.java
@@ -0,0 +1,19 @@
+class Member {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String address;
+    private double salary;
+
+    public Member(String name, int age, String phoneNumber, String address, double salary) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public void printSalary() {
+        System.out.println("Salary " + salary);
+    }
+}
