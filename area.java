55f08e38e582e70fc3a60d97452fb87124095434
Anushka Singh <anushkasingh117733@gmail.com>
2022-02-04 14:09:05 +0530

    Update area.java

--- area.java
+++ area.java
@@ -15,3 +15,20 @@ class Rectangle {
         System.out.println(2 * (breadth + length));
     }
 }
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 5);
+        Rectangle square = new Square(10);
+        rectangle.area();
+        rectangle.perimeter();
+        System.out.println();
+        square.area();
+        square.perimeter();
+    }
