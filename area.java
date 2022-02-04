23a8c360ee52adf45b2e43db378e213df1d6f43e
Anushka Singh <anushkasingh117733@gmail.com>
2022-02-04 14:11:38 +0530

    Update area.java

--- area.java
+++ area.java
@@ -44,3 +44,6 @@ public class Main {
 //Its constructor having parameters for length and breadth is used to initialize length and breadth of the rectangle. Let class 'Square' inherit the 'Rectangle'
 //class with its constructor having a parameter for its side (suppose s) calling the constructor of its parent class as 'super(s,s)'. Print the area and perimeter
 //of a rectangle and a square. 
+//Square(int s){ 
+//Super(s,s) 
+//}
