23c782b74cfe7bdd97c14c6863b407a583f50eff
Anushka Singh <anushkasingh117733@gmail.com>
2022-03-03 14:56:50 +0530

    Create stumanage.java

---
+++ stumanage.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class StudentAL1{
+    int age;
+    int regno;
+    String name;
+    static int maxmarks=100;
+    int marks;
+    void setStudent(){
+    }
+void getStudent(){
+}
+}
+class Main  {
+    public static void main(String a[]){
+        int i=0;
+        int regnumber=0;
+        int mark=0;
